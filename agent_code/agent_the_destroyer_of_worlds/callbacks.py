Fixed outside_map, enabled training with batches and added batch normalization

agent_code/agent_the_destroyer_of_worlds/callbacks.py
@@ -106,43 +106,37 @@ def state_to_features(game_state: dict) -> np.array:
     vision_coordinates = vision_coordinates.T
     vision_coordinates = vision_coordinates.reshape(((reach*2+1)**2, 2))
 
+    outside_map = np.zeros((4, 4))  # coins, crates, bombs, other agents
 
     # --- map with walls (-1), free (0) and crates (1) ---------------------------------------------------------------
+    min_coor = np.min(vision_coordinates, axis = 0)
+    max_coor = np.max(vision_coordinates, axis = 0)
     wall_crates = np.zeros((reach * 2 + 1, reach * 2 + 1)) - 1  # outside of game also -1
-    for coord in vision_coordinates:
-        if not 0 < coord[0] < field.shape[0] or not 0 < coord[1] < field.shape[1]:
-            next
-        else:
-            x = coord[0] - my_pos[0] + reach
-            y = coord[1] - my_pos[1] + reach
-            wall_crates[x, y] = field[coord[0], coord[1]]
+    if 0 <= min(min_coor) and all(max_coor<field.shape):
+        wall_crates[:, :] = field[min_coor[0]:max_coor[0]+1, min_coor[1]:max_coor[1]+1]
+    else:
+        wall_crates[abs(min(0,min_coor[0])):8-abs(min(0, field.shape[0]-2-max_coor[0])),abs(min(0,min_coor[1])):8-abs(min(0, field.shape[1]-2-max_coor[1]))] = field[max(0,min_coor[0]):min(max_coor[0]+1, field.shape[0]), max(0,min_coor[1]):min(max_coor[1]+1, field.shape[1])] 
+
 
     # --- map with explosion (-1) free (0) and coins (1) -------------------------------------------------------------
     explosion_coins = np.zeros((reach * 2 + 1, reach * 2 + 1))
-
     explosion_coord = np.transpose((explosion_map > 0).nonzero())
-    vision_coordinates2 = vision_coordinates[:, np.newaxis, :]
 
-    coins2 = np.asarray(coins)
-    if len(coins2) > 0:
-        coins2 = coins2[np.newaxis, :, :]
-        mask = np.all(vision_coordinates2 == coins2, axis=2)
-        mask = np.where(mask.any(axis=1))
-        if len(mask) > 0:
-            visable = vision_coordinates2[mask] - my_pos + reach
-            explosion_coins[tuple(visable.T)] = 1
+    existing_coords = explosion_coord[(explosion_coord[:, None] == vision_coordinates).all(-1).any(-1)]
+    existing_coords[:, 0] = existing_coords[:,0]-my_pos[0]+reach
+    existing_coords[:, 1] = existing_coords[:,1]-my_pos[1]+reach
+    explosion_coins[existing_coords.T[0], existing_coords.T[1]] = -1
 
-    explosion_coord2 = np.asarray(explosion_coord)
-    if len(explosion_coord2) > 0:
-        explosion_coord2 = explosion_coord2[np.newaxis, :, :]
-        mask = np.all(vision_coordinates2 == explosion_coord2, axis=2)
-        mask = np.where(mask.any(axis=1))
-        if len(mask) > 0:
-            visable = vision_coordinates2[mask] - my_pos + reach
-            explosion_coins[tuple(visable.T)] = -1
+    if len(coins) > 0:
+        coins = np.asarray(coins)
+        existing_coins = coins[(coins[:, None] == vision_coordinates).all(-1).any(-1)]
+        existing_coins[:, 0] = existing_coins[:,0]-my_pos[0]+reach
+        existing_coins[:, 1] = existing_coins[:,1]-my_pos[1]+reach
+        explosion_coins[existing_coins.T[0], existing_coins.T[1]] = 1
 
     # --- map with bomb range (-1), free (0) and opponents (1) --------------------------------------------------------
     bomb_opponents = np.zeros((reach * 2 + 1, reach * 2 + 1))
+    vision_coordinates2 = vision_coordinates[:, np.newaxis, :]
 
     if len(others) > 0:
         others2 = np.array(list(map(itemgetter(3), others)))
@@ -155,6 +149,16 @@ def state_to_features(game_state: dict) -> np.array:
                 bomb_opponents[tuple(visable.T)] = 1
 
     for bomb in bombs:
+        # outside_map getting bombs outside vision field
+        if bomb[0][1] < top:
+            outside_map[2, 0] = 1
+        if bomb[0][1] > bottom:
+            outside_map[2, 1] = 1
+        if bomb[0][0] < left:
+            outside_map[2, 2] = 1
+        if bomb[0][0] > right:
+            outside_map[2, 3] = 1
+
         if any(sum(bomb[0] == i) == 2 for i in vision_coordinates):
             # coordinate of bomb in our vision matrix
             x_bomb = bomb[0][0] - my_pos[0] + reach
@@ -194,30 +198,57 @@ def state_to_features(game_state: dict) -> np.array:
         else: next
 
     # --- scaled down vision outside reach ---------------------------------------------------------------------------
-    outside_map = np.zeros((4, 4))  # coins, crates, bombs, other agents
-
+    
     for i in range(0, field.shape[0]):
         for j in range(0, field.shape[1]):
 
             field_value = field[i, j]
-            if field_value == 0:
-                next
-            else:
-                # coins = 3, crates = 2, bombs and explosions = -1 -> make it to 1, other agents >= 5 -> make it to 0 (index)
-                if field_value == -1:
-                    field_value = 1
-                elif field_value >= 5:
-                    field_value = 0
-
+            if field_value == 1:
                 if j < top:
-                    outside_map[0, field_value] = 1
+                    outside_map[1, 0] += 1
                 if j > bottom:
-                    outside_map[1, field_value] = 1
+                    outside_map[1, 1] += 1
                 if i < left:
-                    outside_map[2, field_value] = 1
+                    outside_map[1, 2] += 1
                 if i > right:
-                    outside_map[3, field_value] = 1
+                    outside_map[1, 3] += 1
 
+    for coin in np.array(coins):
+        if coin[1] < top:
+            outside_map[0, 0] += 1
+        if coin[1] > bottom:
+            outside_map[0, 1] += 1
+        if coin[0] < left:
+            outside_map[0, 2] += 1
+        if coin[0] > right:
+            outside_map[0, 3] += 1
+
+    others2 = np.array(list(map(itemgetter(3), others)))
+    for enemy in others2:
+        if enemy[1] < top:
+            outside_map[3, 0] += 1
+        if enemy[1] > bottom:
+            outside_map[3, 1] += 1
+        if enemy[0] < left:
+            outside_map[3, 2] += 1
+        if enemy[0] > right:
+            outside_map[3, 3] += 1
+    
+    for i in range(0, explosion_map.shape[0]):
+        for j in range(0, explosion_map.shape[1]):
+
+            explosion_value = explosion_map[i, j]
+            if explosion_value != 0:
+                if j < top:
+                    outside_map[2, 0] = 1
+                if j > bottom:
+                    outside_map[2, 1] = 1
+                if i < left:
+                    outside_map[2, 2] = 1
+                if i > right:
+                    outside_map[2, 3] = 1
+
+                    
     wall_crates = torch.tensor([wall_crates.tolist()], device=device)
     explosion_coins = torch.tensor([explosion_coins.tolist()], device=device)
     bomb_opponents = torch.tensor([bomb_opponents.tolist()], device=device)

agent_code/agent_the_destroyer_of_worlds/train.py
@@ -56,7 +56,7 @@ class ReplayMemory(object):
 # setup NN
 class DQN(nn.Module):
 
-    def __init__(self, n_observations, n_actions):
+    def __init__(self, n_actions):
         super(DQN, self).__init__()
         '''self.layer1 = nn.Linear(n_observations, 100)
         self.layer2 = nn.Linear(100, 70)
@@ -72,6 +72,11 @@ class DQN(nn.Module):
         self.hidden13 = nn.Linear(25, 18)
         self.hidden14 = nn.Linear(12, 6)
 
+        self.batch1 = nn.BatchNorm1d(18)
+        self.batch2 = nn.BatchNorm1d(18)
+        self.batch3 = nn.BatchNorm1d(18)
+        self.batch4 = nn.BatchNorm1d(6)
+
         self.hidden2 = nn.Linear(60, 30)
 
         self.output = nn.Linear(30, n_actions)
@@ -86,12 +91,20 @@ class DQN(nn.Module):
         x3 = F.relu(self.input3(x3))
         x4 = F.relu(self.input4(x4))
 
-        x1 = F.relu(self.hidden11(x1.flatten()))
-        x2 = F.relu(self.hidden12(x2.flatten()))
-        x3 = F.relu(self.hidden13(x3.flatten()))
-        x4 = F.relu(self.hidden14(x4)).flatten()
+        x1 = F.relu(self.hidden11(x1.view(x1.size(0), -1)))
+        x2 = F.relu(self.hidden12(x2.view(x2.size(0), -1)))
+        x3 = F.relu(self.hidden13(x3.view(x3.size(0), -1)))
+        x4 = F.relu(self.hidden14(x4.view(x4.size(0), -1)))
 
-        x = torch.cat((x1, x2, x3, x4))
+        #print(x1.size()[0])
+        #print(x4.size())
+        if x1.size()[0] > 1:
+            x1 = self.batch1(x1)
+            x2 = self.batch2(x2)
+            x3 = self.batch3(x3)
+            x4 = self.batch4(x4)
+
+        x = torch.cat((x1, x2, x3, x4), dim=1)
         x = F.relu(self.hidden2(x))
 
         x = self.output(x)
@@ -107,7 +120,7 @@ def setup_training(self):
 
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
-    n_observations = 163  # length of feature
+    #n_observations = 163  # length of feature
     n_actions = 6  # No bombing or waiting for now!
 
     # setup device
@@ -122,9 +135,9 @@ def setup_training(self):
             self.policy_net = pickle.load(file).to(device)
 
     else:
-        self.policy_net = DQN(n_observations, n_actions).to(device)
+        self.policy_net = DQN(n_actions).to(device)
 
-    self.target_net = DQN(n_observations, n_actions).to(device)
+    self.target_net = DQN(n_actions).to(device)
     self.target_net.load_state_dict(self.policy_net.state_dict())
 
     self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=LR, amsgrad=True)
@@ -212,21 +225,24 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
 
     action_batch = torch.cat(batch.action).to(device)
     reward_batch = torch.cat(batch.reward).to(device)
-    state_action_values = torch.zeros((BATCH_SIZE_CORRECTED, 6), device=device)
-    next_state_values = torch.zeros(BATCH_SIZE_CORRECTED, 6, device=device)
 
-    i = 0
-    for s in batch.state:
-        state_action_values[i] = self.policy_net(s[0], s[1], s[2], s[3])
-        i += 1
+    states = list(zip(*batch.state))
+
+    state_action_values = self.policy_net(torch.stack(states[0]), torch.stack(states[1]), torch.stack(states[2]), torch.stack(states[3])).to(device)
     state_action_values = state_action_values.gather(1, action_batch)
 
-    i = 0
-    for s in batch.next_state:
+    input1 = torch.zeros((BATCH_SIZE_CORRECTED, 1, 7, 7), device = device)
+    input2 = torch.zeros((BATCH_SIZE_CORRECTED, 1, 7, 7), device = device)
+    input3 = torch.zeros((BATCH_SIZE_CORRECTED, 1, 7, 7), device = device)
+    input4 = torch.zeros((BATCH_SIZE_CORRECTED, 1, 16), device = device)
+    for i, s in enumerate(batch.next_state):
         with torch.no_grad():
             if non_final_mask[i]:
-                next_state_values[i] = self.target_net(s[0], s[1], s[2], s[3])
-        i += 1
+                input1[i] = s[0]
+                input2[i] = s[1]
+                input3[i] = s[2]
+                input4[i] = s[3]
+    next_state_values = self.target_net(input1, input2, input3, input4).to(device)
     next_state_values = next_state_values.max(1)[0]
 
     # Compute the expected Q values